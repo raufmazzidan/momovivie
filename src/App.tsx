79a71c6cf5987f6d70a164e1d85ea6462f526320
Rauf Mazzidan <Abd. Rauf Jauhari Izza Mazzidan>
2025-04-22 15:55:10 +0700

    feat: setup pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Search from "./pages/Search";
+import Home from "./pages/home1";
+import Search from "./pages/search1";
 
 function App() {
   return (
