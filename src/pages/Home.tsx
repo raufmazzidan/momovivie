5e4aa3d243007af366152ce64cb81f4c72278eaf
Rauf Mazzidan <Abd. Rauf Jauhari Izza Mazzidan>
2025-04-22 15:46:47 +0700

    feat: setup tailwind

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
