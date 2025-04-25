004a1e7dcd5a17ee549b68d2a0ef4e254d95ba8f
Rauf Mazzidan <Abd. Rauf Jauhari Izza Mazzidan>
2025-04-25 22:30:19 +0700

    fix: issue 404 vercel

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/movie/:movieId" element={<MovieDetail />} />
+          <Route path="*" element={<div>Page not found</div>} />
         </Routes>
       </Suspense>
     </QueryClientProvider>
